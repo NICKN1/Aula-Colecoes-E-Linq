30b519e5e3d0455c37c4849c558326ba45318f83
NICKN1 <iranildo.junior@outlook.com>
2022-02-15 21:20:53 -0300

    Aula de Manipulação de Array

---
+++ Colecoes/Helper/OperacoesArrays.cs
@@ -0,0 +1,34 @@
+namespace Colecoes.Helper
+{
+    public class OperacoesArrays
+    {
+        //Metodo para ordenar o array pelo algoritmo Bubble sort
+        public void ordenarBuubleSort(ref int[] array)
+        {
+            //variável temporaria para realizar a troca sem perder a informação armazenada anterior
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                //tem que ter o N -1 para que o array no final não compare com uma posição que não existe
+                for (int j = 0;j < array.Length -1 ; j++)
+                {
+                    //Se o array atual for maior que o proximo >>> Esse +1 indica a proxima posição de j
+                    if (array[j] > array[j +1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+        public void imprimirArray(int[] array)
+        {
+            //Concatenar em uma linha só, exibindo todos os elementos em uma mesma linha
+            var linha = string.Join(", ", array);
+            
+                System.Console.WriteLine(linha);
+            
+        }
+    }
+}
---
+++ Colecoes/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Colecoes.Helper;
+using static System.Console;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciar a classe
+            OperacoesArrays op = new OperacoesArrays();
+            int[] array = new int[5] {3 , 24, 33, 8, 9};
+            
+            WriteLine("Array Original");
+            op.imprimirArray(array);
+
+            op.ordenarBuubleSort(ref array);
+
+            WriteLine("Array ordenado por Bubble Sort");
+            op.imprimirArray(array);
+            /*
+            //Array multidimensional (Matriz) >>> E para instanciar a matriz com valores
+            int[,] arrayIntMatriz = new int[4,2]
+            {
+                //essa é a representação das linhas e colunas da matriz
+                {8, 8 },
+                {7, 7},
+                {6, 6},
+                {4, 5}
+            };
+
+            //Para percorrer as os dados da matriz, criando dois for
+            //getLength é da classe Array, pois pega a dimensão do Array
+            for (int i = 0; i < arrayIntMatriz.GetLength(0); i++)
+            {
+                //outro for para percorrer a coluna
+                for (int j = 0; j < arrayIntMatriz.GetLength(1); j++)
+                {
+                    //Para imprimir os valores das linhas e colunas da matriz
+                    WriteLine(arrayIntMatriz[i, j]);
+                }
+            }
+            
+
+            //Array para capacitar apenas 3 numeros inteiros
+            int[] arrayInteiros = new int[3];
+
+            //Acessando o array através de posições
+            arrayInteiros[0] = 50;
+            arrayInteiros[1] = 70;
+            //Para converter uma string para int
+            arrayInteiros[2] = int.Parse("80");
+
+            //Percorrer o array para imprimir os elementos pelo for
+            /*WriteLine("Imprimindo pelo for");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                WriteLine(arrayInteiros[i]);
+            }
+            //Percorrer o Array com foreach
+            WriteLine("Imprimindo pelo foreach");
+            foreach (int item in arrayInteiros)
+            {
+                WriteLine(item);
+            }
+            */
+        }
+    }
+}
